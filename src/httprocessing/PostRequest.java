34640874cf69a4378284f30d8d26b1db0b2e34a9
Daniel Shiffman <daniel@shiffman.net>
2012-05-23 21:10:46 -0400

    added support for posting a file

--- src/httprocessing/PostRequest.java
+++ src/httprocessing/PostRequest.java
@@ -1,12 +1,22 @@
 package httprocessing;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
@@ -15,7 +25,8 @@ public class PostRequest
 {
 	String url;
 	ArrayList<BasicNameValuePair> nameValuePairs;
-	
+	HashMap<String,File> nameFilePairs;
+
 	String content;
 	HttpResponse response;
 
@@ -23,6 +34,7 @@ public class PostRequest
 	{
 		this.url = url;
 		nameValuePairs = new ArrayList<BasicNameValuePair>();
+		nameFilePairs = new HashMap<String,File>();
 	}
 
 	public void addData(String key, String value) 
@@ -31,14 +43,32 @@ public class PostRequest
 		nameValuePairs.add(nvp);
 	}
 
+	public void addFile(String name, File f) {
+		nameFilePairs.put(name,f);
+	}
+
 	public void send() 
 	{
 		try {
 			DefaultHttpClient httpClient = new DefaultHttpClient();
-
 			HttpPost httpPost = new HttpPost(url);
 
-			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			if (nameFilePairs.isEmpty()) {
+				httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			} else {
+				MultipartEntity mentity = new MultipartEntity();	
+				Iterator it = nameFilePairs.entrySet().iterator();
+			    while (it.hasNext()) {
+			        Map.Entry pair = (Map.Entry)it.next();
+			        String name = (String) pair.getKey();
+			        File f = (File) pair.getValue();
+					mentity.addPart(name, new FileBody(f));
+			    }				
+				for (NameValuePair nvp : nameValuePairs) {
+					mentity.addPart(nvp.getName(), new StringBody(nvp.getValue()));
+				}
+				httpPost.setEntity(mentity);
+			}
 
 			response = httpClient.execute( httpPost );
 			HttpEntity   entity   = response.getEntity();
@@ -47,23 +77,24 @@ public class PostRequest
 			if( entity != null ) EntityUtils.consume(entity);
 
 			httpClient.getConnectionManager().shutdown();
-			
+
 			// Clear it out for the next time
 			nameValuePairs.clear();
+			nameFilePairs.clear();
 
 		} catch( Exception e ) { 
 			e.printStackTrace(); 
 		}
 	}
-	
+
 	/* Getters
 	_____________________________________________________________ */
-	
+
 	public String getContent()
 	{
 		return this.content;
 	}
-	
+
 	public String getHeader(String name)
 	{
 		Header header = response.getFirstHeader(name);
