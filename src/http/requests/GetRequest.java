6f4b44764408d46115e71dfd70f29d91d38316e0
Noah Shibley <archive.of.receipts@gmail.com>
2015-09-18 16:56:17 +0900

    created addHeader method
    You can now add a request header. Useful for API keys.

--- src/http/requests/GetRequest.java
+++ src/http/requests/GetRequest.java
@@ -1,12 +1,17 @@
 package http.requests;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 public class GetRequest
@@ -15,17 +20,28 @@ public class GetRequest
 	String content;
 	HttpResponse response;
 	UsernamePasswordCredentials creds;
+    	ArrayList<BasicNameValuePair> headerPairs;
 
+  
 	public GetRequest(String url) 
 	{
 		this.url = url;
+            headerPairs = new ArrayList<BasicNameValuePair>();
+
 	}
 
 	public void addUser(String user, String pwd) 
 	{
 		creds = new UsernamePasswordCredentials(user, pwd);
-	}
-
+	
+    	}
+    
+    	public void addHeader(String key,String value) {
+        	BasicNameValuePair nvp = new BasicNameValuePair(key,value);
+        	headerPairs.add(nvp);
+        
+    	}  
+      
 	public void send() 
 	{
 		try {
@@ -36,6 +52,13 @@ public class GetRequest
 			if(creds != null){
 				httpGet.addHeader(new BasicScheme().authenticate(creds, httpGet, null));				
 			}
+
+                    	Iterator<BasicNameValuePair> headerIterator = headerPairs.iterator();
+                    	while (headerIterator.hasNext()) {
+                      		BasicNameValuePair headerPair = headerIterator.next();
+                      		httpGet.addHeader(headerPair.getName(),headerPair.getValue());
+                    	}
+  
 
 			response = httpClient.execute( httpGet );
 			HttpEntity entity = response.getEntity();
